dab8d79f1ebdb857c66ed45ce131efd9d8610d97
Marve10s <igrimanigroman@gmail.com>
2024-12-25 19:45:02 +0300

    hotfix for vercel v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
-
 import icon from 'astro-icon';
+import path from 'path';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
+  integrations: [tailwind(), icon()],
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+        '@assets': path.resolve('./src/assets'),
+        '@components': path.resolve('./src/components'),
+        '@layouts': path.resolve('./src/layouts'),
+        '@data': path.resolve('./src/data'),
+        '@types': path.resolve('./src/types')
+      }
+    }
+  }
 });
